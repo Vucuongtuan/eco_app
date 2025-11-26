2346eade582183e54bb0d03998836162589d86b1
VuTuanCuong <vucuongtuan00@gmail.com>
2025-11-26 10:28:44 +0700

    chore: revert page verify from ISR to force dynamic ssr

--- src/app/(app)/[lang]/(auth)/verify/page.tsx
+++ src/app/(app)/[lang]/(auth)/verify/page.tsx
@@ -2,6 +2,8 @@ import { getPayloadInstance } from "@/lib/tryCatch";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
+export const dynamic = "force-dynamic";
+
 export default async function VerifyPage() {
   const cookieStore = await cookies();
   const token = cookieStore.get("payload-token");
