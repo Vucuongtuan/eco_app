b654f94e8754134d642940b8e78ead272353ab47
VuTuanCuong <vucuongtuan00@gmail.com>
2025-12-03 10:32:16 +0700

    feat : add locale field for collection EmailSubscribe

--- src/collections/EmailSubscribe/index.ts
+++ src/collections/EmailSubscribe/index.ts
@@ -1,34 +1,50 @@
 import { CollectionConfig } from "payload";
 
-
-
-export const EmailSubscribe:CollectionConfig={
-    slug:"email-subscribe",
-    labels:{
-        singular:{
-            vi:"Email Subscribe",
-            en:"Email Subscribe"
+export const EmailSubscribe: CollectionConfig = {
+  slug: "email-subscribe",
+  labels: {
+    singular: {
+      vi: "Email Subscribe",
+      en: "Email Subscribe",
+    },
+    plural: {
+      vi: "Email Subscribe",
+      en: "Email Subscribe",
+    },
+  },
+  access: {
+    read: () => true,
+    create: () => true,
+    update: () => true,
+    delete: () => true,
+  },
+  fields: [
+    {
+      name: "email",
+      type: "text",
+      required: true,
+      label: {
+        vi: "Email",
+        en: "Email",
+      },
+    },
+    {
+      name: "locale",
+      type: "select",
+      label: {
+        vi: "Ngôn ngữ",
+        en: "Language",
+      },
+      options: [
+        {
+          label: "Tiếng Việt",
+          value: "vi",
         },
-        plural:{
-            vi:"Email Subscribe",
-            en:"Email Subscribe"
-        }
+        {
+          label: "English",
+          value: "en",
+        },
+      ],
     },
-    access:{
-        read:()=>true,
-        create:()=>true,
-        update:()=>true,
-        delete:()=>true,
-    },
-    fields:[
-          {
-            name:"email",
-            type:"text",
-            required:true,
-            label:{
-                vi:"Email",
-                en:"Email"
-            }
-        }
-    ]
-}
+  ],
+};
